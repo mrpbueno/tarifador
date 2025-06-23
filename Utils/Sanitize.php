e5f8fe43288fc6092ddccf6a563db1215edb4943
Mauro <mrpbueno@gmail.com>
2025-06-22 22:03:47 -0300

    Code improvements

--- Utils/Sanitize.php
+++ Utils/Sanitize.php
@@ -12,7 +12,7 @@ class Sanitize
 {
     public static function string($value)
     {
-        return filter_var($value, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
     }
 
     public static function int($value)
